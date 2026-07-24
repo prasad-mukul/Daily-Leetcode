601b77f36199a9c697182f0b8b601d281f349c6e
Mukul Prasad <mukulprasaad@gmail.com>
2026-07-24 23:54:28 +0530

    Time: 5 ms (80.95%) | Memory: 45.9 MB (53.52%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0) return false;
+        int n = x;
+        int r = 0;
+        while(n != 0){
+            int d = n%10;
+            r=(r*10)+d;
+            n=n/10;
+        }   
+        return (x == r);
+    }
+}

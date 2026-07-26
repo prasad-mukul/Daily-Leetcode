0e9e47a4e1ce96a136f1d5ba35cb8debd4f0c11b
Mukul Prasad <mukulprasaad@gmail.com>
2026-07-26 16:53:59 +0530

    Time: 1 ms (99.97%) | Memory: 71.1 MB (85.99%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+       int n = nums.length;
+       int i = 0;
+       while(i<n){
+            int currentIndex = nums[i] - 1;
+            if(nums[i]>0 && nums[i]<=n && nums[i]!=nums[currentIndex]){
+                int temp = nums[i];
+                nums[i] = nums[currentIndex];
+                nums[currentIndex] = temp;   
+            }
+            else{
+                i++;
+            }
+        }
+        for(int j=0; j<n; j++){
+            if(nums[j] != j+1){
+                return j+1;
+            }
+        }
+        return n+1;
+    }
+}
